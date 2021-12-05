1adcd74a5d6c7b7c52aa3cba83d8ad0df2a92492
Valentin Fontana <valenfontana7@gmail.com>
2021-12-04 22:28:58 -0300

    experience updated

--- profile.js
+++ profile.js
@@ -1,74 +1,74 @@
 export const skills = [
   {
-    skill: "HTML",
-    icon: "html-5.svg",
+    skill: 'HTML',
+    icon: 'html-5.svg',
   },
   {
-    skill: "CSS",
-    icon: "css-3.svg",
+    skill: 'CSS',
+    icon: 'css-3.svg',
   },
   {
-    skill: "SASS",
-    icon: "sass.svg",
+    skill: 'SASS',
+    icon: 'sass.svg',
   },
   {
-    skill: "Javascript",
-    icon: "js.svg",
+    skill: 'Javascript',
+    icon: 'js.svg',
   },
   {
-    skill: "React",
-    icon: "react.svg",
+    skill: 'React',
+    icon: 'react.svg',
   },
   {
-    skill: "NodeJS",
-    icon: "nodejs.svg",
+    skill: 'NodeJS',
+    icon: 'nodejs.svg',
   },
   {
-    skill: "NestJS",
-    icon: "nestjs.svg",
+    skill: 'NestJS',
+    icon: 'nestjs.svg',
   },
   {
-    skill: "Ruby",
-    icon: "ruby.svg",
+    skill: 'Ruby',
+    icon: 'ruby.svg',
   },
   {
-    skill: "PostgreSQL",
-    icon: "postgresql.svg",
+    skill: 'PostgreSQL',
+    icon: 'postgresql.svg',
   },
   {
-    skill: "MongoDB",
-    icon: "mongodb.svg",
+    skill: 'MongoDB',
+    icon: 'mongodb.svg',
   },
   {
-    skill: "Azure DevOps",
-    icon: "azure.svg",
+    skill: 'Azure DevOps',
+    icon: 'azure.svg',
   },
   {
-    skill: "GIT",
-    icon: "git.svg",
+    skill: 'GIT',
+    icon: 'git.svg',
   },
   {
-    skill: "Docker",
-    icon: "docker.svg",
+    skill: 'Docker',
+    icon: 'docker.svg',
   },
 ];
 export const experiences = [
   {
-    title: "Software Engineer at Globallogic",
+    title: 'Software Engineer at Globallogic',
     description:
-      "Project for an important American Fintech in which I take part: //- Software Design and Development. /- Interface Setup and Testing. /- Troubleshooting. //Stack we use: Ruby, NodeJS and ReactJS. //We work in an Azure environment with Continuous Integration and Continuous Delivery, using Agile Methodologies.",
-    from: "Nov 2020",
+      'Project for an important American Fintech in which I take part: //- Software Design and Development. /- Interface Setup and Testing. /- Troubleshooting. //Stack we use: Ruby, NodeJS and ReactJS. //We work in an Azure environment with Continuous Integration and Continuous Delivery, using Agile Methodologies.',
+    from: 'Nov 2020',
     to: (
       <>
-        - Present (<b>10</b> months)
+        - Present (<b>1</b> year)
       </>
     ),
   },
   {
-    title: "HENRY app (practices)",
+    title: 'HENRY app (practices)',
     description:
-      "I took part in the development of an educative portal, which allows the users to login, see their classes and contents and also chat with another users. // The project was carried out with the following technologies: HTML, CSS, JavaScript, React, React Native, Redux, NodeJS, GraphQL and PostgreSQL.",
-    from: "Oct 2020",
+      'I took part in the development of an educative portal, which allows the users to login, see their classes and contents and also chat with another users. // The project was carried out with the following technologies: HTML, CSS, JavaScript, React, React Native, Redux, NodeJS, GraphQL and PostgreSQL.',
+    from: 'Oct 2020',
     to: (
       <>
         - Nov 2020 (<b>1</b> month)
@@ -76,10 +76,10 @@ export const experiences = [
     ),
   },
   {
-    title: "E-commerce (practices)",
+    title: 'E-commerce (practices)',
     description:
-      "With some colleagues from the Bootcamp, we developed a footwear Ecommerce, which allows the user to see products, register, login, add to cart, give the product a review, upload files, finish the purchase, and other functionalities. // The project was carried out with the following technologies: HTML, CSS, JavaScript, ReactJS, Redux, NodeJS, Sequelize ORM, ExpressJS and PostgreSQL.",
-    from: "Sep 2020",
+      'With some colleagues from the Bootcamp, we developed a footwear Ecommerce, which allows the user to see products, register, login, add to cart, give the product a review, upload files, finish the purchase, and other functionalities. // The project was carried out with the following technologies: HTML, CSS, JavaScript, ReactJS, Redux, NodeJS, Sequelize ORM, ExpressJS and PostgreSQL.',
+    from: 'Sep 2020',
     to: (
       <>
         - Oct 2020 (<b>1</b> month)
@@ -87,10 +87,10 @@ export const experiences = [
     ),
   },
   {
-    title: "Tutor",
+    title: 'Tutor',
     description:
-      "I supported and helped new students on learning the topics we studied in the Bootcamp. After their classes, we reviewed the concepts and helped them to understand the exercises.",
-    from: "Sep 2020",
+      'I supported and helped new students on learning the topics we studied in the Bootcamp. After their classes, we reviewed the concepts and helped them to understand the exercises.',
+    from: 'Sep 2020',
     to: (
       <>
         - Nov 2020 (<b>3</b> months)
@@ -100,57 +100,57 @@ export const experiences = [
 ];
 export const projects = [
   {
-    name: "Meraki Ecommerce",
-    description: "Natural Cosmetics shop.",
-    image: "meraki.png",
-    url: "https://merakisanpedro.herokuapp.com/",
-    github: "https://github.com/valenfontana7/meraki",
+    name: 'Meraki Ecommerce',
+    description: 'Natural Cosmetics shop.',
+    image: 'meraki.png',
+    url: 'https://merakisanpedro.herokuapp.com/',
+    github: 'https://github.com/valenfontana7/meraki',
   },
   {
-    name: "Notes App",
-    description: "Create, edit and delete shared notes.",
-    image: "app_de_notas.png",
-    url: "https://app-de-notas.vercel.app/",
-    github: "https://github.com/valenfontana7/app-de-tareas",
+    name: 'Notes App',
+    description: 'Create, edit and delete shared notes.',
+    image: 'app_de_notas.png',
+    url: 'https://app-de-notas.vercel.app/',
+    github: 'https://github.com/valenfontana7/app-de-tareas',
   },
   {
-    name: "Google Clone",
-    description: "Google official page clone.",
-    image: "google_clone.png",
-    url: "https://google-clone-weld-psi.vercel.app/",
-    github: "https://github.com/valenfontana7/google_clone",
+    name: 'Google Clone',
+    description: 'Google official page clone.',
+    image: 'google_clone.png',
+    url: 'https://google-clone-weld-psi.vercel.app/',
+    github: 'https://github.com/valenfontana7/google_clone',
   },
   {
-    name: "Rick & Morty App",
-    description: "Rick & Morty application.",
-    image: "rick-n-morty.png",
-    url: "https://rick-n-morty-app.vercel.app/",
-    github: "https://github.com/valenfontana7/rick_and_morty_app",
+    name: 'Rick & Morty App',
+    description: 'Rick & Morty application.',
+    image: 'rick-n-morty.png',
+    url: 'https://rick-n-morty-app.vercel.app/',
+    github: 'https://github.com/valenfontana7/rick_and_morty_app',
   },
   {
-    name: "Harry Potter Wiki",
-    description: "Harry Potter Wiki",
-    image: "hp_fanpage.png",
-    url: "https://harrypotter-app.vercel.app/",
-    github: "https://github.com/valenfontana7/harry_potter_fanpage",
+    name: 'Harry Potter Wiki',
+    description: 'Harry Potter Wiki',
+    image: 'hp_fanpage.png',
+    url: 'https://harrypotter-app.vercel.app/',
+    github: 'https://github.com/valenfontana7/harry_potter_fanpage',
   },
 ];
 export const posts = [
   {
-    title: "React",
+    title: 'React',
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur a accusantium, magnam corrupti dignissimos quos, labore maiores, eos nesciunt quae ad fuga ratione. Quia ipsum fuga animi distinctio cum delectus?",
-    imageURL: "react.png",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur a accusantium, magnam corrupti dignissimos quos, labore maiores, eos nesciunt quae ad fuga ratione. Quia ipsum fuga animi distinctio cum delectus?',
+    imageURL: 'react.png',
   },
   {
-    title: "HTML",
+    title: 'HTML',
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur a accusantium, magnam corrupti dignissimos quos, labore maiores, eos nesciunt quae ad fuga ratione. Quia ipsum fuga animi distinctio cum delectus?",
-    imageURL: "html.jpeg",
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur a accusantium, magnam corrupti dignissimos quos, labore maiores, eos nesciunt quae ad fuga ratione. Quia ipsum fuga animi distinctio cum delectus?',
+    imageURL: 'html.jpeg',
   },
 ];
 export const contact = {
-  phone: "+543329511319",
-  email: "valenfontana7@gmail.com",
-  location: "Buenos Aires, Argentina",
+  phone: '+543329511319',
+  email: 'valenfontana7@gmail.com',
+  location: 'Buenos Aires, Argentina',
 };
